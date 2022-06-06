f827573ec39cb544dfd9b876bee0f5574fe3f504
Carlos Armando Marcano Vargas <70811425+carlosm27@users.noreply.github.com>
2022-06-06 16:55:36 -0400

    Add files via upload
    Add migrations folder, tables and piccolo_app files

---
+++ blacksheep_tutorial/sql_app/piccolo_migrations/2022-06-04T17-09-41-748757.py
@@ -0,0 +1,60 @@
+from piccolo.apps.migrations.auto.migration_manager import MigrationManager
+from piccolo.columns.column_types import Integer
+from piccolo.columns.column_types import Varchar
+from piccolo.columns.indexes import IndexMethod
+
+
+ID = "2022-06-04T17:09:41:748757"
+VERSION = "0.74.4"
+DESCRIPTION = ""
+
+
+async def forwards():
+    manager = MigrationManager(
+        migration_id=ID, app_name="sql_app", description=DESCRIPTION
+    )
+
+    manager.add_table("Expense", tablename="expense")
+
+    manager.add_column(
+        table_class_name="Expense",
+        tablename="expense",
+        column_name="amount",
+        db_column_name="amount",
+        column_class_name="Integer",
+        column_class=Integer,
+        params={
+            "default": 0,
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Expense",
+        tablename="expense",
+        column_name="description",
+        db_column_name="description",
+        column_class_name="Varchar",
+        column_class=Varchar,
+        params={
+            "length": 255,
+            "default": "",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    return manager

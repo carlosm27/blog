dd3070d3af224291bd127655c35df2fcc7b33fd5
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-06-30 14:56:21 -0400

    create database.go

---
+++ cockroachdbGorillaMux/model/database.go
@@ -0,0 +1,30 @@
+package model
+
+import (
+	"log"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func SetupDB() (*gorm.DB, error) {
+
+	dsn := "postgresql://carlosm27:-l6ISkOmWHx79KSEENXiSA@free-tier14.aws-us-east-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Dwide-okapi-2719"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect database", err)
+	}
+
+	var now time.Time
+	db.Raw("SELECT NOW()").Scan(&now)
+
+	log.Println(now)
+
+	if err = db.AutoMigrate(&Expenses{}); err != nil {
+		log.Println(err)
+	}
+
+	return db, err
+
+}

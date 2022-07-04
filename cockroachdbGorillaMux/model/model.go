c09f44550596a26369eab86611f465102d0331fa
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-07-04 10:51:06 -0400

    Added UUID field

--- cockroachdbGorillaMux/model/model.go
+++ cockroachdbGorillaMux/model/model.go
@@ -6,6 +6,8 @@ import (
 
 type Expenses struct {
 	gorm.Model
+	ID          uint    `json:"id"`
+	IdExpense   string  `json:"id_expense" gorm:"type:uuid;default:uuid_generate_v4()"`
 	Amount      float64 `json:"amount"`
 	Description string  `json:"description"`
 	Category    string  `json:"category"`

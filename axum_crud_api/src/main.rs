07babc615c1167b4a7873888d7389b5553726e64
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-08-15 22:31:05 -0400

    Add sqlx connections

--- axum_crud_api/src/main.rs
+++ axum_crud_api/src/main.rs
@@ -1,23 +1,44 @@
 use axum::{
-    routing::{get}, Router,
+    extract::{Extension},routing::{get, post}, Router,
 };
 
+use sqlx::postgres::PgPoolOptions;
 use std::net::SocketAddr;
+use std::fs;
+use anyhow::Context;
+mod models;
+mod controllers;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
+
+    let env = fs::read_to_string(".env").unwrap();
+    let (key, database_url) = env.split_once('=').unwrap();
+
+
+    assert_eq!(key, "DATABASE_URL");
 
     tracing_subscriber::fmt::init();
 
+    let pool = PgPoolOptions::new()
+    .max_connections(50)
+    .connect(&database_url)
+    .await
+    .context("could not connect to database_url")?;
+
     let app = Router::new()
-        .route("/hello", get(hello));
+        .route("/hello", get(hello))
+        .route("/tasks", get(controllers::task::all_tasks))
+        .route("/task", post(controllers::task::new_task))
+        .layer(Extension(pool));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("Listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
-        .await
-        .unwrap();
+        .await?;
+
+        Ok(())
 
 }
 

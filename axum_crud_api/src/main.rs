d5f7d4da831a23372c37f0caa98bdf966ee4dc35
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-08-15 21:49:43 -0400

    Update main.rs

--- axum_crud_api/src/main.rs
+++ axum_crud_api/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::{get}, Router,
+};
+
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/hello", get(hello));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("Listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+
+}
+
+async fn hello() -> &'static str {
+    "Hello, World!"
 }

6c0e0b5598fcb6cfe32ddd51a53e3b16b386a76e
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-08-17 21:09:03 -0400

    Add PUT and DELETE endpoints

--- axum_crud_api/src/controllers/task.rs
+++ axum_crud_api/src/controllers/task.rs
@@ -1,9 +1,10 @@
 use axum::response::IntoResponse;
-//use axum::extract::Path;
+use axum::extract::Path;
 use axum::http::StatusCode;
 
 use axum::{Extension, Json};
 use sqlx::PgPool;
+use serde_json::json;
 
 use crate::{
     models::task
@@ -31,4 +32,35 @@ pub async fn new_task(Json(task): Json<task::NewTask>, Extension(pool): Extensio
         .unwrap();
 
     (StatusCode::OK, Json(task))
+}
+
+pub async fn task(Path(id):Path<i32>, Extension(pool): Extension<PgPool>) -> impl IntoResponse {
+    let sql = "SELECT * FROM task where values id=$1".to_string();
+
+    let task: task::Task = sqlx::query_as(&sql).bind(id).fetch_one(&pool).await.unwrap();
+
+    (StatusCode::OK, Json(task))
+}
+
+pub async fn update_task(Path(id): Path<i32>, Json(task): Json<task::UpdateTask>, Extension(pool): Extension<PgPool>) -> impl IntoResponse {
+
+    sqlx::query("UPDATE FROM task SET task=$1 WHERE id=$2")
+        .bind(&task.task)
+        .bind(id)
+        .execute(&pool)
+        .await
+        .unwrap();
+    
+    (StatusCode::OK, Json(task))    
+}
+
+pub async fn delete_task(Path(id): Path<i32>, Extension(pool): Extension<PgPool>) -> impl IntoResponse {
+
+    sqlx::query("DELETE FROM task WHERE id=$1")
+        .bind(id)
+        .execute(&pool)
+        .await
+        .unwrap();
+    
+        (StatusCode::OK, Json(json!({"msg": "Task Deleted"})))
 }

ec730af78383b47c452e2eedb7facb3552b873d5
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-08-19 14:39:54 -0400

    Add TraceLayer

--- axum_crud_api/src/main.rs
+++ axum_crud_api/src/main.rs
@@ -6,6 +6,8 @@ use sqlx::postgres::PgPoolOptions;
 use std::net::SocketAddr;
 use std::fs;
 use anyhow::Context;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod errors;
 mod models;
@@ -20,7 +22,13 @@ async fn main() -> anyhow::Result<()> {
 
     assert_eq!(key, "DATABASE_URL");
 
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("tower_http=trace")
+                .unwrap_or_else(|_| "example_tracing_aka_logging=debug,tower_http=debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
     let pool = PgPoolOptions::new()
     .max_connections(50)
@@ -35,7 +43,8 @@ async fn main() -> anyhow::Result<()> {
         .route("/task/:id",get(controllers::task::task))
         .route("/task/:id", put(controllers::task::update_task))
         .route("/task/:id", delete(controllers::task::delete_task))
-        .layer(Extension(pool));
+        .layer(Extension(pool))
+        .layer(TraceLayer::new_for_http());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("Listening on {}", addr);

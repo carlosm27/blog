c58c5f0676919c56ba799d86cdc90596f3e4808d
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-08-21 13:57:20 -0400

    Add custom errors to UPDATE and DELETE controllers

--- axum_crud_api/src/controllers/task.rs
+++ axum_crud_api/src/controllers/task.rs
@@ -4,7 +4,8 @@ use axum::http::StatusCode;
 
 use axum::{Extension, Json};
 use sqlx::PgPool;
-use serde_json::{json};
+use serde_json::{json, Value};
+
 
 use crate::{
     models::task,
@@ -15,18 +16,19 @@ use crate::{
 pub async fn all_tasks(Extension(pool): Extension<PgPool>) -> impl IntoResponse {
     let sql = "SELECT * FROM task ".to_string();
 
-    let task = sqlx::query_as::<_, task::Task>(&sql)
+   let task = sqlx::query_as::<_, task::Task>(&sql)
         .fetch_all(&pool)
         .await.unwrap();
-        //.map_err(|_| {CustomError::InternalServerError});
 
-    (StatusCode::OK, Json(task))
+    (StatusCode::OK,Json(task))
 }
 
 
-pub async fn new_task(Json(task): Json<task::NewTask>, Extension(pool): Extension<PgPool>) ->Result <(StatusCode, Json<task::NewTask>), CustomError> {
+pub async fn new_task(Json(task): Json<task::NewTask>, Extension(pool): Extension<PgPool>) -> Result <(StatusCode, Json<task::NewTask>), CustomError> {
     
-
+    if task.task.is_empty() {
+        return Err(CustomError::BadRequest)
+    }
     let sql = "INSERT INTO task (task) values ($1)";
 
     let _ = sqlx::query(&sql)
@@ -34,8 +36,8 @@ pub async fn new_task(Json(task): Json<task::NewTask>, Extension(pool): Extensio
         .execute(&pool)
         .await
         .map_err(|_| {
-            CustomError::BadRequest
-        });
+            CustomError::InternalServerError
+        })?;
 
     Ok((StatusCode::CREATED, Json(task)))
 }
@@ -53,25 +55,41 @@ pub async fn task(Path(id):Path<i32>, Extension(pool): Extension<PgPool>) -> Res
     Ok(Json(task))  
 }
 
-pub async fn update_task(Path(id): Path<i32>, Json(task): Json<task::UpdateTask>, Extension(pool): Extension<PgPool>) -> impl IntoResponse {
+pub async fn update_task(Path(id): Path<i32>, Json(task): Json<task::UpdateTask>, Extension(pool): Extension<PgPool>) -> Result <(StatusCode, Json<task::UpdateTask>), CustomError> {
+
+
+    let sql = "SELECT * FROM task where id=$1".to_string();
+
+    let _find: task::Task = sqlx::query_as(&sql).bind(id).fetch_one(&pool).await
+        .map_err(|_| {
+            CustomError::TaskNotFound
+        })?;
 
     sqlx::query("UPDATE task SET task=$1 WHERE id=$2")
         .bind(&task.task)
         .bind(id)
         .execute(&pool)
-        .await
-        .unwrap();
+        .await;
+        
     
-    (StatusCode::OK, Json(task))    
+    Ok((StatusCode::OK, Json(task)))
 }
 
-pub async fn delete_task(Path(id): Path<i32>, Extension(pool): Extension<PgPool>) -> impl IntoResponse {
+pub async fn delete_task(Path(id): Path<i32>, Extension(pool): Extension<PgPool>) -> Result <(StatusCode, Json<Value>), CustomError> {
+
+
+    let _find: task::Task = sqlx::query_as("SELECT * FROM task where id=$1").bind(id).fetch_one(&pool).await
+        .map_err(|_| {
+            CustomError::TaskNotFound
+        })?;
 
     sqlx::query("DELETE FROM task WHERE id=$1")
         .bind(id)
         .execute(&pool)
         .await
-        .unwrap();
+        .map_err(|_| {
+            CustomError::TaskNotFound
+        })?;
     
-        (StatusCode::OK, Json(json!({"msg": "Task Deleted"})))
+        Ok((StatusCode::OK, Json(json!({"msg": "Task Deleted"}))))
 }

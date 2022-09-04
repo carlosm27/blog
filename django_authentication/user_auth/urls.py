7e4d8eb59ac9117b55fd8bededc34159abee62ab
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-09-03 21:18:44 -0400

    Add password_change url

--- django_authentication/user_auth/urls.py
+++ django_authentication/user_auth/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home_page, name="home"),
     path('signup/', views.SignUp.as_view(), name="signup"),
-    path('login/', views.Login.as_view(), name="login")
+    path('login/', views.Login.as_view(), name="login"),
+    path('password_change', views.PasswordChange.as_view(), name ="password_change"),
     
 ]
